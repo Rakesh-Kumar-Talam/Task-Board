5210ca3b04bbfb16c649711bf1111a17b10d1cc1
Talam-Rakesh-Kumar <rakesh.22bce7907@vitapstudent.ac.in>
2025-09-22 21:36:28 +0530

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://talamrakeshkumar123_db_user:7hZQVBfACzXtt8aJ@taskboard.xw9rxyf.mongodb.net/?retryWrites=true&w=majority&appName=TaskBoard');
+    const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://talamrakeshkumar123_db_user:7hZQVBfACzXtt8aJ@taskboard.xw9rxyf.mongodb.net/?retryWrites=true&w=majority&appName=TaskBoard';
+    
+    const conn = await mongoose.connect(mongoURI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('Database connection error:', error.message);
     console.log('Server will continue running without database connection...');
-    console.log('Please whitelist your IP address in MongoDB Atlas: https://cloud.mongodb.com/');
+    console.log('Please check your MongoDB Atlas connection string and IP whitelist settings.');
     // Don't exit the process - let the server continue running
   }
 };
